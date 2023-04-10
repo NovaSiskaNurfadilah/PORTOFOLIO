6a843706364330d8924ead27514f326a33237240
Nova Siska Nurfadilah <12150124481@students.uin-suska.ac.id>
2023-04-10 15:45:19 +0700

    PORTOFOLIO

---
+++ src/views/About.vue
@@ -0,0 +1,40 @@
+<template>
+
+  <section id="home">
+    <div class="container w-100 vh-100 d-flex justify-content-center align-items-center">
+
+      <div class="content text-center">
+        <img src="../assets/S.png"
+        HEIGHT="400" WIDTH="250">
+          <h1 class="display-3">Selamat datang di websiteku! </h1>
+          <p class="lead">"website design ini dibuat untuk keperluan portofolio saya sendiri sebagai Security Engineer dengan pengalaman 1 tahun di
+            perusahaan teknologi global. Memiliki keahlian dalam melakukan konfigurasi firewall di seluruh jaringan, analisi dan perbaikan malware, dan memahami keamanan cloud serta bahasa pemograman.
+            Berdedikasi untuk menjaga integritas sistem, file. dan data perusahaan.” </p>
+
+          <div class="d-flex">
+            <a href=""></a>
+          </div>
+        </div>
+      </div>
+
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+<style>
+.container{
+  background: dodgerblue;
+}
+</style>
+
+<style>
+.d-flex {
+  text-align: center;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <!--banner image-->
+
+  <div class="banner-image w-100 vh-100 d-flex justify-content-center align-items-center">
+    <div class="images">
+      <img src="../assets/N(1).png" class="rounded"
+           height="300" width="300px">
+    </div>
+    <div class="content text-center">
+      <h1 class="text-white">Welcome To Nova Siska Nurfadilah</h1>
+      <h2>
+        <a href="https://instagram.com/nopaicaai_19?igshid=YmMyMTA2M2Y=" target="_blank" style="color: white" rel="noopener">Instagram</a> +
+        <a href="https://github.com/NovaSiskaNurfadilah" target="_blank" style="color: white" rel="noopener">Github</a>
+      </h2>
+    </div>
+  </div>
+
+
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+
+<style>
+.banner-image{
+  background: dodgerblue;
+}
+
+</style>
